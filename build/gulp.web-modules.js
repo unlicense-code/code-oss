cff81e723c47093947b5e82f1cdf17ecf7093bf2
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-12-03 07:07:14 +0100

    Create gulp.web-modules.js

---
+++ build/gulp.web-modules.js
@@ -0,0 +1,2 @@
+// Creates web-modules see: https://github.com/lemanschik.com/web-modules
+// Run gulp.web-components.js to create a dist build (splits this into worker and view module compositions. )
