36ea8f2b1272174215cc1d32373addc9c91ab6ec
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2024-11-07 10:49:23 +0100

    Add: playground example from vscode-web

---
+++ packages/examples/code-oss-playground/build.js
@@ -0,0 +1,96 @@
+/* eslint-disable header/header */
+import process from "process";
+import child_process from "child_process";
+import fs from "fs";
+import fse from "fs-extra";
+import { version } from "../../code-oss-dev/package.json";
+
+const vscodeVersion = version.split("-")[0];
+
+function error(msg) {
+	console.info("\x1b[31merror %s\x1b[0m", msg);
+}
+function ok(msg) {
+	console.info("\x1b[32m%s\x1b[0m", msg);
+}
+function note(msg) {
+	console.info("\x1b[90m%s\x1b[0m", msg);
+}
+function exec(cmd, opts) {
+	console.info("\x1b[36m%s\x1b[0m", cmd);
+	return child_process.execSync(cmd, opts);
+}
+
+const requiredTools = ["node", "git", "python"];
+note(`required tools ${JSON.stringify(requiredTools)}`);
+for (const tool of requiredTools) {
+	try {
+		child_process.execSync(`${tool} --version`, { stdio: "ignore" });
+	} catch (e) {
+		error(`"${tool}" is not available.`);
+		process.exit(1);
+	}
+}
+ok("required tools installed");
+
+// TODO: Reuse code-oss-dev/build/preinstall.js uses 20.18
+// const node_version_out = child_process.execSync(`node -v`);
+// const node_version = node_version_out.toString().trim();
+// if (node_version < "v20.0") {
+// 	error(`Want node > 20. Got "${node_version}"`);
+// 	process.exit(1);
+// }
+
+// TODO: Running already in mono repo it is under ../../
+// if (!fs.existsSync("vscode")) {
+// 	note("cloning vscode");
+// 	exec(
+// 		`git clone --depth 1 https://github.com/microsoft/vscode.git -b ${vscodeVersion}`,
+// 		{
+// 			stdio: "inherit",
+// 		}
+// 	);
+// } else {
+// 	ok("vscode already installed");
+// 	note("delete vscode folder to clone again");
+// }
+
+note("changing directory to vscode");
+process.chdir("../../");
+
+if (!fs.existsSync("node_modules")) {
+	exec("npm install --ignore-scripts", {
+		stdio: "inherit",
+		env: {
+			...process.env,
+			ELECTRON_SKIP_BINARY_DOWNLOAD: 1,
+			PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD: 1,
+		},
+	});
+} else {
+	ok("node_modules exists. Skipping install");
+}
+
+// Use simple workbench
+note("copying workbench file");
+// fs.copyFileSync(
+// 	"../workbench.ts",
+// 	"src/vs/code/browser/workbench/workbench.ts"
+// );
+
+// Compile
+note("starting compile");
+exec("npm run gulp vscode-web-min", { stdio: "inherit" });
+ok("compile completed");
+
+// Extract compiled files
+if (fs.existsSync("../dist")) {
+	note("cleaning ../dist");
+	fs.rmdirSync("../dist", { recursive: true });
+} else {
+	ok("../dist did not exist. No need to clean");
+}
+
+fs.mkdirSync("../dist");
+fse.copySync("../vscode-web", "../dist");
+ok("copied ../vscode-web to ../dist");
